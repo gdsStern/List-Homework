e425f748d259cc86b2cc5e7ac4a45cb6c75e5970
User <git@github.com:gdsStern/HelloWorld.git>
2024-04-25 16:31:14 +0300

    files

---
+++ src/main/java/pro/sky/List/Homework/ListHomeworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.List.Homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListHomeworkApplication.class, args);
+	}
+
+}
